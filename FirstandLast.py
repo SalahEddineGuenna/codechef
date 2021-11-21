4d1958519155b1fd1c662cc527bc19186764f47d
Salah Eddine GUENNA <salah.eddine1768@gmail.com>
2021-11-21 14:08:51 +0100

    FLAST

--- FirstandLast.py
+++ FirstandLast.py
@@ -4,9 +4,7 @@ res = []
 for i in range(T):
     x = input("")
     M = [int(m) for m in x]
-    result = 0
-    result = M[0] + M[-1]
-    res.append(result)
+    res.append(M[0] + M[-1])
 
 for i in res:
     print(i)
