b574e07561628d35397fee40aab00161a09c9f52
Salah Eddine GUENNA <salah.eddine1768@gmail.com>
2021-11-21 19:52:58 +0100

    HS08TEST

--- ATM.py
+++ ATM.py
@@ -1,5 +1,5 @@
 def withdraw(x, y):
-    if(x % 5  == 0 & x <= y):
+    if(x % 5  == 0 & x + 0.50 <= y):
         y = y - (x + 0.50)
         return y
     return y
@@ -8,6 +8,5 @@ def withdraw(x, y):
 x, y = input("").split()
 n = int(x)
 m = float(y)
-if(n <= m):
-    n = withdraw(n, m)
-print(m)
+n = withdraw(n, m)
+print(n)
